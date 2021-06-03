411974227288c1690a570de8527856855d15caf9
MarvinJanssen <MarvinJanssen@users.noreply.github.com>
2021-06-03 00:55:38 -0700

    Revert "fix: temporarily disable registrations from the main page"
    This reverts commit 0f1cb4a8ffa1cc81ef84fd0758fd1d9ae4b1b215.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -95,11 +95,8 @@
 	<div>
 		<PromoBanner/>
 		<h1>{$t('page.index.heading')}</h1>
-		{#if 0}
-			<DomainSearchField small={$width_705} on:submit={result => result.detail.available && goto('/get/'+result.detail.name+'.btc')}/>
-			<p>{@html $t('page.index.sub_heading')}</p>
-		{/if}
-		<p>Thanks for your interest in registering a .btc domain - we're quickly working on a few improvements to make your experience faster and smoother and we'll be back online within a few hours!</p>
+		<DomainSearchField small={$width_705} on:submit={result => result.detail.available && goto('/get/'+result.detail.name+'.btc')}/>
+		<p>{@html $t('page.index.sub_heading')}</p>
 	</div>
 </header>
 <article>
